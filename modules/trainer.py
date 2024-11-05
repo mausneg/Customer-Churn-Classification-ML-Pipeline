e56e0e3a3f7009adde7e977af5dbe8bc0f0e9a8f
mausneg <maulanasuryanegara123@gmail.com>
2024-11-05 10:57:40 +0800

    add tuner modules

--- modules/trainer.py
+++ modules/trainer.py
@@ -3,55 +3,12 @@ import tensorflow_transform as tft
 from tfx.components.trainer.fn_args_utils import FnArgs
 import os
 from transform import CATEGORICAL_FEATURE_KEY, LABEL_KEY, NUMERIC_FEATURE_KEY, transformed_name
+from tuner import input_fn
 
 LABEL_KEY = 'Churn'
+BATCH_SIZE = 512
+EPOCHS = 50
 
-
-def gzip_reader_fn(filenames):
-    return tf.data.TFRecordDataset(filenames=filenames, compression_type='GZIP')
-
-def input_fn(file_pattern, tf_transform_output, num_epocs, batch_size=64)->tf.data.Dataset:
-    transform_feature_spec = tf_transform_output.transformed_feature_spec().copy()
-
-    def _to_dense(features, labels):
-        if isinstance(labels, tf.sparse.SparseTensor):
-            labels = tf.sparse.to_dense(labels, default_value=0)
-        return features, labels
-
-    dataset = tf.data.experimental.make_batched_features_dataset(
-        file_pattern=file_pattern,
-        batch_size=batch_size,
-        features=transform_feature_spec,
-        reader=gzip_reader_fn,
-        num_epochs=num_epocs,
-        label_key=transformed_name(LABEL_KEY)
-    )
-    dataset = dataset.map(_to_dense)
-    return dataset
-
-def model_builder():
-    input_features = []
-    for key in NUMERIC_FEATURE_KEY:
-        input_features.append(tf.keras.layers.Input(shape=(1,), name=transformed_name(key)))
-    for key, dim in CATEGORICAL_FEATURE_KEY.items():
-        input_features.append(tf.keras.layers.Input(shape=(dim,), name=transformed_name(key)))
-    inputs = tf.keras.layers.concatenate(input_features)
-    print(inputs)  
-    x = tf.keras.layers.Dense(64, activation='relu')(inputs)
-    x = tf.keras.layers.Dropout(0.5)(x)
-    x = tf.keras.layers.Dense(32, activation='relu')(x)
-    x = tf.keras.layers.Dropout(0.5)(x)
-    outputs = tf.keras.layers.Dense(1, activation='sigmoid')(x)
-
-    model = tf.keras.Model(inputs=input_features, outputs=outputs)
-    model.compile(
-        loss='binary_crossentropy',
-        optimizer=tf.keras.optimizers.Adam(0.001),
-        metrics=tf.keras.metrics.BinaryAccuracy()
-    )
-    model.summary()
-    tf.keras.utils.plot_model(model, show_shapes=True)
-    return model
 
 def _get_serve_tf_examples_fn(model, tf_transform_output):
     model.tft_layer = tf_transform_output.transform_features_layer()
@@ -76,7 +33,27 @@ def _get_serve_tf_examples_fn(model, tf_transform_output):
 
     return serve_tf_examples_fn
 
-def run_fn(fn_args: FnArgs) -> None:
+def model_builder(hp):
+    inputs = []
+    for key in NUMERIC_FEATURE_KEY:
+        inputs.append(tf.keras.layers.Input(shape=(1,), name=transformed_name(key)))
+    for key, dim in CATEGORICAL_FEATURE_KEY.items():
+        inputs.append(tf.keras.layers.Input(shape=(dim,), name=transformed_name(key)))
+    x = tf.keras.layers.concatenate(inputs)
+    for i in range(hp['num_layers']):
+        x = tf.keras.layers.Dense(units=hp['units_' + str(i)], activation='relu')(x)
+        x = tf.keras.layers.Dropout(rate=hp['dropout_' + str(i)])(x)
+    outputs = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(hp['learning_rate']),
+        loss='binary_crossentropy',
+        metrics=[tf.keras.metrics.BinaryAccuracy()]
+    )
+    return model
+
+def run_fn(fn_args: FnArgs):
+    hp = fn_args.hyperparameters['values']
     log_dir = os.path.join(fn_args.model_run_dir, 'logs')
     tensorboard_callback = tf.keras.callbacks.TensorBoard(
         log_dir=log_dir, 
@@ -88,30 +65,17 @@ def run_fn(fn_args: FnArgs) -> None:
         patience=40,
         restore_best_weights=True
     )
-    plateau_callback = tf.keras.callbacks.ReduceLROnPlateau(
-        monitor='val_loss',
-        factor=0.1,
-        min_delta=0.01,
-        patience=5,
-        min_lr=0.00001
-    )
-    model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-        filepath=fn_args.serving_model_dir,
-        monitor='val_loss',
-        mode='max',
-        save_best_only=True
-    )
-    tf_transform_output = tft.TFTransformOutput(fn_args.transform_output)
-    train_set = input_fn(fn_args.train_files, tf_transform_output, num_epocs=50, batch_size=512)
-    val_set =  input_fn(fn_args.eval_files, tf_transform_output, num_epocs=50, batch_size=512)
-    model = model_builder()
+    tf_transform_output = tft.TFTransformOutput(fn_args.transform_graph_path)
+    train_set = input_fn(fn_args.train_files, tf_transform_output, num_epocs=EPOCHS, batch_size=BATCH_SIZE)
+    val_set =  input_fn(fn_args.eval_files, tf_transform_output, num_epocs=EPOCHS, batch_size=BATCH_SIZE)
+    model = model_builder(hp)
     model.fit(
         train_set,
         steps_per_epoch=fn_args.train_steps,
         validation_data=val_set,
         validation_steps=fn_args.eval_steps,
-        callbacks=[tensorboard_callback, early_stopping_callback, plateau_callback, model_checkpoint_callback],
-        epochs=50
+        callbacks=[tensorboard_callback, early_stopping_callback],
+        epochs=hp['tuner/epochs'],
     )
     signatures = {
         'serving_default':
---
+++ modules/tuner.py
@@ -0,0 +1,99 @@
+from typing import Any, Dict, NamedTuple, Text
+import keras_tuner as kt
+import tensorflow as tf
+from keras_tuner.engine import base_tuner
+import tensorflow_transform as tft
+from tfx.components.trainer.fn_args_utils import FnArgs
+import os
+from transform import CATEGORICAL_FEATURE_KEY, NUMERIC_FEATURE_KEY, LABEL_KEY, transformed_name
+
+EPOCHS = 50
+BATCH_SIZE = 512
+
+TunerFnResult = NamedTuple("TunerFnResult", [
+    ("tuner", base_tuner.BaseTuner),
+    ("fit_kwargs", Dict[Text, Any]),
+])
+
+def gzip_reader_fn(filenames):
+    return tf.data.TFRecordDataset(filenames=filenames, compression_type='GZIP')
+
+def input_fn(file_pattern, tf_transform_output, num_epocs, batch_size=64)->tf.data.Dataset:
+    transform_feature_spec = tf_transform_output.transformed_feature_spec().copy()
+
+    def _to_dense(features, labels):
+        if isinstance(labels, tf.sparse.SparseTensor):
+            labels = tf.sparse.to_dense(labels, default_value=0)
+        return features, labels
+
+    dataset = tf.data.experimental.make_batched_features_dataset(
+        file_pattern=file_pattern,
+        batch_size=batch_size,
+        features=transform_feature_spec,
+        reader=gzip_reader_fn,
+        num_epochs=num_epocs,
+        label_key=transformed_name(LABEL_KEY)
+    )
+    dataset = dataset.map(_to_dense)
+    return dataset
+
+def build_model(hp):
+    inputs = []
+    for key in NUMERIC_FEATURE_KEY:
+        inputs.append(tf.keras.layers.Input(shape=(1,), name=transformed_name(key)))
+
+    for key, dim in CATEGORICAL_FEATURE_KEY.items():
+        inputs.append(tf.keras.layers.Input(shape=(dim,), name=transformed_name(key)))
+
+    x = tf.keras.layers.concatenate(inputs)
+    for i in range(hp.Int('num_layers', 1, 3)):
+        x = tf.keras.layers.Dense(units=hp.Int('units_' + str(i), min_value=32, max_value=512, step=32), activation='relu')(x)
+        x = tf.keras.layers.Dropout(rate=hp.Float('dropout_' + str(i), 0.1, 0.5, step=0.1))(x)
+
+    outputs = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(
+            hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
+        ),
+        loss='binary_crossentropy',
+        metrics=[tf.keras.metrics.BinaryAccuracy()]
+    )
+    return model
+
+
+def tuner_fn(fn_args: FnArgs):
+    tf_transform_output = tft.TFTransformOutput(fn_args.transform_graph_path)
+    train_dataset = input_fn(fn_args.train_files, tf_transform_output, EPOCHS, BATCH_SIZE)
+    eval_dataset = input_fn(fn_args.eval_files, tf_transform_output, EPOCHS, BATCH_SIZE)
+
+    early_stopping_callback = tf.keras.callbacks.EarlyStopping(
+        monitor='val_loss',
+        min_delta=0.01, 
+        patience=40,
+        restore_best_weights=True
+    )
+
+    tuner = kt.Hyperband(
+        hypermodel=build_model,
+        objective=kt.Objective('val_binary_accuracy', direction='max'),
+        max_epochs=EPOCHS,
+        factor=3,
+        directory=fn_args.working_dir,
+        project_name='churn',
+    )
+
+    tuner.oracle.max_trials = 10
+
+    return TunerFnResult(
+        tuner=tuner,
+        fit_kwargs={
+            'x': train_dataset,
+            'steps_per_epoch': fn_args.train_steps,
+            'validation_data': eval_dataset,
+            'validation_steps': fn_args.eval_steps,
+            'callbacks': [
+                early_stopping_callback, 
+            ]
+        }
+    )
